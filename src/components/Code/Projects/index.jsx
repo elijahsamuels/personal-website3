133de9e086a1ffe6d56782b7709c32885385b55f
Elijah Samuels <elijahsamuels@gmail.com>
2025-12-14 22:16:33 -0700

    clean up

--- src/components/Code/Projects/index.jsx
+++ src/components/Code/Projects/index.jsx
@@ -1,4 +1,3 @@
-// import { useId } from "react";
 import ProjectsObject from "./ProjectsObject";
 import "./Projects.css";
 import { SiGitlab, SiGithub, SiNetlify, SiHeroku, SiApplemusic } from "react-icons/si";
@@ -7,7 +6,6 @@ import { Parallax } from "react-scroll-parallax";
 
 document.title = `Elijah Samuels | Projects`;
 
-// Helper function: prodLinkReturn
 const prodLinkReturn = (linkProps) => {
   let size = "1em";
   if (linkProps.includes("netlify")) {
@@ -21,14 +19,11 @@ const prodLinkReturn = (linkProps) => {
   }
 };
 
-// Component: ProjectsList (formerly ProjectsReturn)
 const ProjectsList = () => {
   return ProjectsObject.map((project, index) => {
     const startScroll = index * 100;
     const endScroll = startScroll + 300;
-    const translateYValue = typeof window !== 'undefined' 
-      ? [`0px`, `-${window.innerHeight / 4}px`] 
-      : [`0px`, `-100px`];
+    const translateYValue = typeof window !== "undefined" ? [`0px`, `-${window.innerHeight / 4}px`] : [`0px`, `-100px`];
 
     return (
       <Parallax
@@ -56,11 +51,7 @@ const ProjectsList = () => {
               {project.github_link && <SiGithub size="1em" />}
               {project.gitlab_link && <SiGitlab size="1em" />}
             </a>
-            <a
-              href={project.prod_link}
-              target="_blank"
-              rel="noreferrer"
-              className="link social-media-icon prod-link">
+            <a href={project.prod_link} target="_blank" rel="noreferrer" className="link social-media-icon prod-link">
               {prodLinkReturn(project.prod_link)}
             </a>
           </div>
@@ -70,20 +61,9 @@ const ProjectsList = () => {
   });
 };
 
-
-// Main Component: Projects
 const Projects = () => {
-  // useId is not strictly needed here if we use project.name as the key 
-  // in the ProjectsList component, which is a better practice.
-  // let id = useId(); 
-
-  // The rest of the component's state/logic can remain here.
-
   return (
     <div>
-      {/* 3. Now, call ProjectsList as a standard React component.
-        This ensures it is only defined once and rendered correctly. 
-      */}
       <ProjectsList />
     </div>
   );
